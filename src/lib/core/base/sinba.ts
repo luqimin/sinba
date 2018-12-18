8374bfc536f082bfbe58a821e84991c8e690f98f
QiminLu <qilu@163.com>
2018-12-18 10:53:35 +0800

    type: 修正类型错误

--- src/lib/core/base/sinba.ts
+++ src/lib/core/base/sinba.ts
@@ -1,7 +1,9 @@
 import { BaseContext } from 'koa';
+import { SinbaCore } from '../core';
 
 interface SinbaContext extends BaseContext {
     [key: string]: any;
+    app: SinbaCore;
 }
 
 export { SinbaContext };
